4d2b6a994c732d49380fbc0d1e98014ee89b61e6
frenchvargulf <marta.s.czarnecka@gmail.com>
2022-03-25 13:36:47 +0100

    Style the subnav, define routing strategy for discord subpages

--- Xtrades.XtradesMarketingPage/src/app/pages/discord/faq/faq.component.ts
+++ Xtrades.XtradesMarketingPage/src/app/pages/discord/faq/faq.component.ts
@@ -3,9 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-faq',
   templateUrl: './faq.component.html',
-  styleUrls: ['./faq.component.css'],
+  styleUrls: ['./faq.component.scss'],
 })
 export class FaqComponent {
   pattern =
     '{BTO/STC | STO/BTC} {ticker} {strikePrice}{C/P} {expDate} @ {contractPrice}';
+  secondPattern = '{BTO/STC | STO/BTC} {ticker} @ {price}';
 }
--- Xtrades.XtradesMarketingPage/src/app/pages/discord/server-guide/server-guide.component.ts
+++ Xtrades.XtradesMarketingPage/src/app/pages/discord/server-guide/server-guide.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-server-guide',
   templateUrl: './server-guide.component.html',
-  styleUrls: ['./server-guide.component.css']
+  styleUrls: ['./server-guide.component.scss'],
 })
-export class ServerGuideComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class ServerGuideComponent {}
