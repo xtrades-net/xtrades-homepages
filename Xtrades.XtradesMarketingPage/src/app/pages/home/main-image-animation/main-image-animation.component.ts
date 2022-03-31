49c721b745589b41d2e0fa37ae68bb734bca4396
frenchvargulf <marta.s.czarnecka@gmail.com>
2022-03-31 14:26:03 +0200

    Remove leftover link and force animation width and height

--- Xtrades.XtradesMarketingPage/src/app/pages/home/main-image-animation/main-image-animation.component.ts
+++ Xtrades.XtradesMarketingPage/src/app/pages/home/main-image-animation/main-image-animation.component.ts
@@ -1,17 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { AnimationItem } from 'lottie-web';
+import { Component } from '@angular/core';
 import { AnimationOptions } from 'ngx-lottie';
 
 @Component({
   selector: 'app-main-image-animation',
   template: ` <ng-lottie [options]="options"></ng-lottie> `,
 })
-export class MainImageAnimationComponent implements OnInit {
+export class MainImageAnimationComponent {
   options: AnimationOptions = {
     path: '/assets/main-homepage-animation.json',
   };
-
-  constructor() {}
-
-  ngOnInit(): void {}
 }
