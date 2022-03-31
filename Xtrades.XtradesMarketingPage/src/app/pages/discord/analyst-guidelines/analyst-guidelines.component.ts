e252499612776c6d4d5bbce74732bdb80f52bd98
frenchvargulf <marta.s.czarnecka@gmail.com>
2022-03-31 14:29:44 +0200

    Remove discord module

--- Xtrades.XtradesMarketingPage/src/app/pages/discord/analyst-guidelines/analyst-guidelines.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-analyst-guidelines',
-  templateUrl: './analyst-guidelines.component.html',
-})
-export class AnalystGuidelinesComponent {}
--- Xtrades.XtradesMarketingPage/src/app/pages/discord/server-guide/server-guide.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-server-guide',
-  templateUrl: './server-guide.component.html',
-  styleUrls: ['./server-guide.component.scss'],
-})
-export class ServerGuideComponent {}
