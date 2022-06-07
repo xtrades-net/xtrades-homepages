fd1b210cc795da8c128579ca73a97a9ff8122800
frenchvargulf <marta.s.czarnecka@gmail.com>
2022-06-07 22:56:03 +0200

    Initial commit

---
+++ Xtrades.XtradesMarketingPage/src/app/pages/discord/discord-guidelines/where-do-i-start.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-where-do-i-start',
+  templateUrl: './where-do-i-start.component.html',
+})
+export class WhereDoIStartComponent {}
---
+++ Xtrades.XtradesMarketingPage/src/app/pages/discord/general-rules/general-rules.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-general-rules',
+  templateUrl: './general-rules.component.html',
+})
+export class GeneralRulesComponent {}
