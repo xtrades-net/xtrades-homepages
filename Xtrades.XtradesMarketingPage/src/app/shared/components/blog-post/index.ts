cc602e2c8725c7ba67a07b324fd6064bf7786754
Ruchika-Agilis <118760885+Ruchika-Agilis@users.noreply.github.com>
2022-11-25 20:17:09 +0530

    Update index.ts

--- Xtrades.XtradesMarketingPage/src/app/shared/components/blog-post/index.ts
+++ Xtrades.XtradesMarketingPage/src/app/shared/components/blog-post/index.ts
@@ -11,4 +11,5 @@ export * from './blog-post-10/blog-post-10.component';
 export * from './blog-post-11/blog-post-11.component';
 export * from './blog-post-12/blog-post-12.component';
 export * from './blog-post-13/blog-post-13.component';
+export * from './blog-post-14/blog-post-14.component';
 
