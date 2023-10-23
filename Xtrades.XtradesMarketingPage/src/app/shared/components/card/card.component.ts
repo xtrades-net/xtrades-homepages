c24db1ae19e208ee600ecb8b63cae1f71f36d351
Yulia Masliak <110036110+yuliaMasliak@users.noreply.github.com>
2023-10-23 17:05:00 +0300

    feat: update text

--- Xtrades.XtradesMarketingPage/src/app/shared/components/card/card.component.ts
+++ Xtrades.XtradesMarketingPage/src/app/shared/components/card/card.component.ts
@@ -8,14 +8,21 @@ import { ScreenService } from '@core/screen.service';
 })
 export class CardComponent implements OnInit {
   @Input() title = '';
-  @Input() subtitle = '';
+  @Input() subtitle: string | string[] = '';
   @Input() titleImgSrc = '';
   @Input() backgroundNoise = false;
   @Input() squeezed = false;
   @Input() iconPath = '';
   @Input() iconAlt = '';
+  @Input() link = '';
+  subtitleArray: string[] = [];
 
   constructor(public screenService: ScreenService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    if (Array.isArray(this.subtitle) && this.subtitle.length > 0) {
+      this.subtitleArray = this.subtitle;
+      this.subtitle = '';
+    }
+  }
 }
