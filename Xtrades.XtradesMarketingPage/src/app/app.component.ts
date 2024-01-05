754c2ecc4f2825119e1b27472281df74766555b1
Yulia Masliak <110036110+yuliaMasliak@users.noreply.github.com>
2024-01-05 13:01:38 +0200

    feat: add intercom messenger

--- Xtrades.XtradesMarketingPage/src/app/app.component.ts
+++ Xtrades.XtradesMarketingPage/src/app/app.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import * as AOS from 'aos';
 
+declare global {
+  interface Window {
+    Intercom: any;
+  }
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -16,6 +22,12 @@ export class AppComponent implements OnInit {
       duration: 500,
       offset: 40,
       once: true,
+    });
+    window.Intercom('boot', {
+      app_id: "kz9zp2wu",
+      email: '',
+      user_id: 'Xtrades Homepage User',
+      created_at: new Date(),
     });
   }
 }
